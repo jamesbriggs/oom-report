a15883d793c2c23960ae160e9788a18e8a4b71fc
James Briggs <james@actionmessage.com>
2019-03-02 16:55:23 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # oom-report
 Summarize RAM consumption per process from syslog OOM reports
+
+```
+Usage: perl oom_report.pl < file.txt
+```
+where file.txt contains an OOM listing extracted from /var/log/syslog. You must include the column header row.
+
+
+Here's an example of running oom_report.pl on a 16 GB server:
+
+```
+$ perl oom_report.pl < myoom.txt
+
+[..]
+    salt-minion =         264,981
+        dockerd =         297,009
+docker-containe =         442,017
+          agent =         584,720
+          java3 =         877,946
+          java1 =       1,205,023
+          nginx =       2,926,052
+          java2 =       5,608,659
+
+          total =      13,427,264
+```
+Notes:
+
+* Current kernels kill the largest user of RAM by default, so in this case the process named java2.
+* Note that 13 GB is accounted from 16 GB. Likely the remainder is used by the kernel itself.
+
+License: Apache 2
+
---
+++ oom_report.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+#
+# Program: oom_report.pl
+# Author: James Briggs, USA
+# Purpose: parse syslog OOM and report on memory use
+# Env: Perl5
+# Date: 2019 02 02
+# Usage: perl oom_report.pl <oom.txt
+# Note:
+
+   my $DEBUG = 0;
+   my $hdr = ' tgid ';
+
+   my %ps;
+
+   my $i = 0;
+
+   while (<>) { # scan for syslog OOM columm headers
+      chomp;
+      next if /^$/;
+
+      $i = index($_, $hdr);
+
+      last if $i > -1;
+   }
+
+   if (!$i) {
+      print STDERR "error: header $hdr not founnd.\n";
+      exit 1;
+   }
+
+   my $n = 1;
+   while (<>) { # scan for processes in OOM listing
+      next if /^$/;
+
+      if (/Out of memory:/) { # end of OOM listing processes
+         print;
+         <>;
+         print;
+         last;
+      }
+
+      chomp;
+
+      # skip over to columns of interest ...
+      $_ = substr($_, $i+length($hdr));
+
+      print substr($_, $i+length($hdr)) if $DEBUG;
+
+      my ($vm, $rss, undef, undef, undef, undef, $ps) = split;
+
+      if ($ps eq 'java') {
+         $ps .= $n;
+         $n++;
+      }
+
+      $ps{$ps} += $vm + $rss;
+   }
+
+   for my $ps (sort { $ps{$a} <=> $ps{$b} } keys %ps) {
+      print leftpad($ps) . " = " . leftpad(commify($ps{$ps})) . "\n";
+      $total += $ps{$ps};
+   }
+
+   print "\n";
+   print leftpad("total") . " = " . leftpad(commify($total)) . "\n";
+
+   exit;
+
+sub commify {
+    my $text = reverse $_[0];
+    $text =~ s/(\d\d\d)(?=\d)(?!\d*\.)/$1,/g;
+    return scalar reverse $text;
+}
+
+sub leftpad {
+   sprintf("%15s", shift);
+}
+
